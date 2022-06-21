b99c1e847f70959f72f53a32aa1c9f4c192df913
Empa <emanuel@empa.xyz>
2022-06-21 21:38:31 +0200

    Remove stuff, simplify

--- grammar.js
+++ grammar.js
@@ -24,61 +24,32 @@ module.exports = grammar({
   name: "saft",
 
   rules: {
-    source_file: $ => repeat($.statement),
+    source_file: $ => repeat($.expression),
 
-    statement: $ => seq("[", $._inner_statement, "]"),
+    expression: $ =>
+      choice(
+        seq("(", $._inner_expression, ")"),
+        $.int,
+        $.float,
+        $.str,
+        $.bool,
+        $.identifier
+      ),
 
-    _inner_statement: $ => choice($.def),
+    _inner_expression: $ => choice($.expression, $.call),
 
-    def: $ => seq("def", $.identifier, $.expression),
+    call: $ => seq("(", $.identifier, repeat($.expression), ")"),
 
     identifier: $ => /[A-Za-z_][A-Za-z_0-9]*/,
 
-    primitive: $ => choice($.int, $.float, $.str, $.bool),
     int: $ => /\d+/,
     float: $ => /(?:\d+\.\d*)|(?:\d*\.\d+)/,
     str: $ => /"(?:[^"\\]|\\.)*"/,
     bool: $ => choice("true", "false"),
 
-    _binary_inner_expression: $ =>
-      choice($.binary_expression, $.unary_expression, $.expression),
-    binary_expression: $ =>
-      choice(
-        ...[
-          ["+", PREC.term],
-          ["-", PREC.term],
-          ["*", PREC.factor],
-          ["/", PREC.factor],
-          ["<", PREC.compare],
-          ["<=", PREC.compare],
-          [">", PREC.compare],
-          [">=", PREC.compare],
-          ["==", PREC.compare],
-          ["!=", PREC.compare],
-          ["or", PREC.or],
-          ["and", PREC.and],
-        ].map(([op, precedence]) =>
-          prec.left(
-            precedence,
-            seq($._binary_inner_expression, op, $._binary_inner_expression)
-          )
-        ),
-        ...[["^", PREC.exp]].map(([op, precedence]) =>
-          prec.right(
-            precedence,
-            seq($._binary_inner_expression, op, $._binary_inner_expression)
-          )
-        )
-      ),
     unary_expression: $ =>
-      prec.left(PREC.unary, seq(choice("not", "+", "-"), $.expression)),
-
-    expression: $ =>
-      choice(
-        seq("(", repeat1($.expression), ")"),
-        seq("(", choice($.binary_expression, $.unary_expression), ")"),
-        $.identifier,
-        $.primitive
-      ),
+      prec.left(PREC.unary, seq(choice("+", "-"), $.expression)),
   },
+
+  precedences: $ => [[$.call, $.expression]],
 });
